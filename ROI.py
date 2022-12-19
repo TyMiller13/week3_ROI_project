f9f2419ee472af0c6d781fa603c793cf7559ab76
Tyler Miller <tylermiller1321@outlook.com>
2022-12-18 21:46:56 -0600

    first commit, built skeleton of program

---
+++ ROI.py
@@ -0,0 +1,27 @@
+# WATCH BIGGER POCKETS VIDEO FOR ROI 
+
+class ReturnOnInvestment:
+
+    def __init__(self,monthly_income=0,monthly_expenses=0,monthly_cashflow=0,annual_cashflow=0,investments=0,roi=0):
+        self.monthly_income = monthly_income
+        self.monthly_expenses = monthly_expenses
+        self.monthly_cashflow = monthly_cashflow
+        self.annual_cashflow = annual_cashflow
+        self.investments = investments
+        self.roi = roi
+        pass
+#Method for income
+    #get income from all sources
+#Method for expenses
+    #get expenses from all sources
+#Method for cashflow
+    #get cash flow from income subtracting the expenses
+#Method for cash on cash ROI
+    #get ROI by dividing your annual cash flow by the total amount invested and return
+
+def run_roi():
+
+    my_roi = ReturnOnInvestment()
+    while True:
+        pass
+
